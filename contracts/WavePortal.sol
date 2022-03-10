ca2eaa8cef7d99567fbd77a8e86d1c80db3f3b7f
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2022-03-10 18:16:47 -0300

    feat(init): project setup

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Hey hey hey, you might be a contract but you are not smart ;)");
+    }
+}
+
